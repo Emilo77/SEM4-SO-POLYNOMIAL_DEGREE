36aecc8440834ac9eb6be08c7796f759e66ba77e
Kamil Bugała <kamilbugala9@gmail.com>
2022-03-25 11:36:16 +0100

    Small changes

--- poly.asm
+++ poly.asm
@@ -5,15 +5,15 @@ global polynomial_degree:
 ;All_equal
 ;       equal <arr> <length> <result>
 
+;%endmacro
 
 ; ZMIENNE
 ; ilość segmentów na pojedynczy bigNum
 ; ilość wszystkich segmentów do trzymania wszystkich bigNumów (alokacja na stosie)
 ; iterator do chodzenia po całych bigNumach
 ; iterator do chodzenia wewnątrz pojedynczego bigNuma
-;
 
-;%endmacro
+
 
 section .text
 
@@ -22,6 +22,21 @@ polynomial_degree:
     div r12, 64
     inc r12 ;ilość segmentów potrzebna do reprezentacji bignuma
 
+    mov rcx, r12
+    mul rcx, rdi ;ustawienie countera na ilość wszystkich segmentów
+
+push_stack_loop:
+    push ... ;pushowanie czegoś
+    loop push_stack_loop
+
+
+
+
+
+
+
+
 ; sprawdzenie, czy wszystkie są równe (i równe 0)
 ; odejmowanie bigNumów, wstawienie na odpowiednie miejsca
+; w loopie robić x razy push na stosie
 
