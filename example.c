5c2a68fda1cee5cfd79608778d4fc4c019f4fac2
Kamil Bugała <kamilbugala9@gmail.com>
2022-03-25 22:32:57 +0100

    Working values and stack initialization

--- example.c
+++ example.c
@@ -1,12 +1,14 @@
+#include <stdint-gcc.h>
 #include "stdio.h"
 #include "polynomial_degree.c"
+#include "limits.h"
 
 int main() {
     const int* str;
     size_t n = 100;
-    int res = polynomial_degree(str, n);
+    uint64_t res = polynomial_degree(str, n);
 
 
-    printf("res = %d\n", res);
+    printf("res = %lu\n", res);
 
 }
--- polynomial_degree.asm
+++ polynomial_degree.asm
@@ -13,21 +13,28 @@ global polynomial_degree:
 ; iterator do chodzenia po całych bigNumach
 ; iterator do chodzenia wewnątrz pojedynczego bigNuma
 
-
-
 section .text
 
 polynomial_degree:
-    mov eax, 50
+    mov rax, rsi
+    add rax, 32  ; najgorszy przypadek to 2^32, -2^32, 2^32, ...
+    shr rax, 6 ; dzielenie przez 64, otrzymanie podłogi z ilości segmentów
+    inc rax
+    mov r11, rax ; r11 - ilość segmentów potrzebna do reprezentacji bignuma r11_max = 2^26
+
+    mul rsi
+;    dec rax ; odjęcie 1, bo będzie co najwyżej n-1 bignumów, ODKOMENTOWAĆ JEŚLI ZADZIAŁA
+    mov r12, rax ; r12 - ilość wszystkich segmentów do iteracji, r12_max = 2^58
+
+    mov r14, rsi ; r14 - iterator zewnętrzny do chodzenia po bignumach
+    mov r15, r11 ; r15 - iterator wewnętrzny do chodzenia po segmnetach wewnątrz bignuma
+
+
+    shl rax, 3 ; ilość wszystkich bitów, o które należy przesunąć wskaźnik stosu, 8 * r12
+    mov r10, rax
+    sub rsp, rax
+    add rsp, rax
     ret
-;    mov r12, qword rdi ; rdi - ilość wszystkich bignumów
-;    div r12, 64
-;    inc r12 ; ilość segmentów potrzebna do reprezentacji bignuma
-;
-;    mov rcx, r12
-;    mul rcx, rdi ; ustawienie countera na ilość wszystkich segmentów
-;    mov r13, rcx ; ilość wszystkich segmentów
-;
 ;push_stack_loop:
 ;    push ... ;pushowanie czegoś
 ;    loop push_stack_loop
