4377fbf8735069d581cb13c142f3f1566675dd46
Kamil Bugała <kamilbugala9@gmail.com>
2022-03-26 19:40:59 +0100

    Working moving initial arr to stack

--- example.c
+++ example.c
@@ -13,56 +13,56 @@ int polynomial_degree(int const *y, size_t n);
 
 
 void fillArr(int *arr, size_t n, int k) {
-    for (int i = 0; i < n - 1; i++) {
-        arr[i] = k;
+    for (int i = 0; i < n; i++) {
+        arr[i] = -i - 2;
     }
 }
 
 
 int main() {
 
-//    size_t n = 50;
-//    int *arr = malloc(sizeof(int) * n);
-//    fillArr(arr, n, 0);
-//    int res = polynomial_degree(arr, n);
-//    printf("res = %d\n", res);
-//    free(arr);
+    size_t n = 50;
+    int *arr = malloc(sizeof(int) * n);
+    fillArr(arr, n, 0);
+    int res = polynomial_degree(arr, n);
+    printf("res = %d\n", res);
+    free(arr);
 
 
-    test(1, 0, { 777 });
-    test(2, 0, {5, 5});
-    test(1, -1, {0});
-    test(1, 0, {-2});
-    test(1, 0, {-1});
-    test(1, 0, {47});
-    test(1, 0, {15});
-    test(1, 0, {5});
-    test(1, 0, {28});
-    test(1, 0, {28});
-    test(1, 0, {30});
-    test(1, 0, {8});
-    test(1, 0, {39});
-    test(1, 0, {27});
-    test(1, 0, {57});
-    test(2, 0, {39, 39});
-    test(2, 0, {23, 23});
-    test(2, 0, {37, 37});
-    test(2, 0, {54, 54});
-    test(2, 0, {48, 48});
-    test(2, 0, {37, 37});
-    test(2, 0, {17, 17});
-    test(2, 0, {45, 45});
-    test(2, 0, {32, 32});
-    test(2, 0, {19, 19});
-    test(3, 0, {48, 48, 48});
-    test(3, 0, {33, 33, 33});
-    test(3, 0, {2, 2, 2});
-    test(3, 0, {11, 11, 11});
-    test(3, 0, {28, 28, 28});
-    test(3, 0, {5, 5, 5});
-    test(3, 0, {25, 25, 25});
-    test(3, 0, {15, 15, 15});
-    test(3, 0, {50, 50, 50});
-    test(3, 0, {37, 37, 37});
+//    test(1, 0, { 777 });
+//    test(2, 0, {5, 5});
+//    test(1, -1, {0});
+//    test(1, 0, {-2});
+//    test(1, 0, {-1});
+//    test(1, 0, {47});
+//    test(1, 0, {15});
+//    test(1, 0, {5});
+//    test(1, 0, {28});
+//    test(1, 0, {28});
+//    test(1, 0, {30});
+//    test(1, 0, {8});
+//    test(1, 0, {39});
+//    test(1, 0, {27});
+//    test(1, 0, {57});
+//    test(2, 0, {39, 39});
+//    test(2, 0, {23, 23});
+//    test(2, 0, {37, 37});
+//    test(2, 0, {54, 54});
+//    test(2, 0, {48, 48});
+//    test(2, 0, {37, 37});
+//    test(2, 0, {17, 17});
+//    test(2, 0, {45, 45});
+//    test(2, 0, {32, 32});
+//    test(2, 0, {19, 19});
+//    test(3, 0, {48, 48, 48});
+//    test(3, 0, {33, 33, 33});
+//    test(3, 0, {2, 2, 2});
+//    test(3, 0, {11, 11, 11});
+//    test(3, 0, {28, 28, 28});
+//    test(3, 0, {5, 5, 5});
+//    test(3, 0, {25, 25, 25});
+//    test(3, 0, {15, 15, 15});
+//    test(3, 0, {50, 50, 50});
+//    test(3, 0, {37, 37, 37});
 
 }
--- polynomial_degree.asm
+++ polynomial_degree.asm
@@ -1,9 +1,13 @@
 global polynomial_degree:
 
+
+
 ;Define macro
 ;Three arguments
 ;All_equal
 ;       equal <arr> <length> <result>
+;
+
 
 ;%endmacro
 
@@ -21,7 +25,6 @@ polynomial_degree:
     shr rax, 6 ; dzielenie przez 64, otrzymanie podłogi z ilości segmentów
     inc rax
     mov r11, rax ; r11 - ilość segmentów potrzebna do reprezentacji bignuma r11_max = 2^26
-
     mul rsi
 ;    dec rax ; odjęcie 1, bo będzie co najwyżej n-1 bignumów, ODKOMENTOWAĆ JEŚLI ZADZIAŁA
     mov r12, rax ; r12 - ilość wszystkich segmentów do iteracji, r12_max = 2^58
@@ -34,11 +37,11 @@ polynomial_degree:
     mov r10, rax ; r10 - zarezerwowane miejsce na stos
     sub rsp, rax
 
-    mov rcx, 0 ; counter od 1 do n - 1
 
     cmp rsi, 1 ; rsi = n
     je check_zero
 
+    mov rcx, 0 ; counter od 1 do n - 1
     mov r9, rsi
     dec r9
 
@@ -61,25 +64,59 @@ check_zero:
     jmp finish
 
 not_equal:
-    mov r14, 0 ; ustawienie countera bignumów
+    mov r9, rsi
+    mov r14, 0 ; ustawienie countera bigNumów
 
 loop_conv_32:
-    mov rax, r14
-    mul r15
+    mov rcx, r11 ; counter do chodzenia po wewnętrznych segmentach pojedynczego bigNuma
+    sub rcx, 1
+
+    mov rax, r14 ;
+    mul r11 ; ilość segmentów dla jednego bigNuma
     mov r13, rax ; ustawienie r13 na iloczyn r14 i r15
 
-    mov rax, [rsp + 8 * r13] ; wstawienie
-    movsxd rax, dword [rdi + r14]
+    movsxd rax, dword [rdi + 4 * r14]
+    mov [rsp + 8 * r13], rax; wstawienie
+;    cmp rcx, 0
+;    jmp dont_fill
+
+;    xor rbx, rbx
+;    cmp rax, 0
+;    jge fill_rest
+;    not rbx
+;    jmp fill_rest
+;
+;fill_rest:
+;    jmp finish
+;    mov r8, rcx
+;    add r8, r13
+;    mov [rsp + 8 * r8], rbx
+;    loop fill_rest
+
+;dont_fill:
+
     inc r14
     cmp r14, rsi
     jne loop_conv_32
 
+
+
 substract:
+    mov r14, 0 ; counter po zewnętrznych segmentach wszsystkich bigNumów
+    mov r15, 0 ; counter po wewnętrznych segmentach pojednynczego bigNuma
+
+
+;    mov rax, qword [rsp + 8 * (r14 + r15)]
+;    sub rax, qword [rsp + 8 * (r14 + r13)]
+
 
 
 finish:
+;    mov rax, qword [rsp + 0]
+    mov rax, [rsp + 16]
     add rsp, r10
     ret
+
 ; sprawdzenie, czy wszystkie są równe (i równe 0)
 ; odejmowanie bigNumów, wstawienie na odpowiednie miejsca
 ; w loopie robić x razy push na stosie
